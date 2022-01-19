adb0341e5cea1db876cebb7a95687925859db8d3
AswinBarath <aswin2001barath@gmail.com>
2022-01-19 14:44:55 +0530

    Q51 Combination Sum I from Recursion

--- SDE_Sheet/src/Recursion/Q51_CominationSum_I.java
+++ SDE_Sheet/src/Recursion/Q51_CominationSum_I.java
@@ -1,5 +1,22 @@
 package Recursion;
 
+/*
+ * Leetcode problem link: https://leetcode.com/problems/combination-sum/
+ * 
+ * Use Recursion: Whenever you see Combination or subsequences
+ * 
+ * Apply "Pick and Don't Pick" idea
+ * 
+ * Time Complexity: O(2^t * k) --> Exponential
+ * 					where 2^t is no. of times we explore combinations 
+ * 							k is no. of times we add combination DS to answer DS  
+ * 
+ * Space Complexity: O(k * x) --> Hypothetical 
+ * 					where k is the average length of DS
+ * 						  x is the no. of combination
+ * 
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,31 +39,32 @@ public class Q51_CominationSum_I {
 
 	public static List<List<Integer>> combinationSum(int[] candidates, int target) {
 		List<List<Integer>> answer = new ArrayList<>();
-		combinationSum(candidates, target, 0, answer, new ArrayList<Integer>());
+		combinationSum(0, candidates, target, answer, new ArrayList<Integer>());
 		return answer;
 	}
 
-	public static void combinationSum(int[] candidates, int target, int i, List<List<Integer>> answer,
+	public static void combinationSum(int ind, int[] candidates, int target, List<List<Integer>> answer,
 			List<Integer> choices) {
 
-		if (target == 0) {
-			answer.add(choices);
-			choices = new ArrayList<Integer>();
+		// Base case
+		if (ind == candidates.length) {
+			if (target == 0) {
+				answer.add(new ArrayList<>(choices)); // This addition of choices to answer is O(size) --> Linear, not constant
+				// we add choices only if they sum up to target
+			}
 			return;
 		}
 
-		if (target < 0 || i >= candidates.length) {
-			choices = new ArrayList<Integer>();
-			return;
+		// pick recursive case
+		if (candidates[ind] <= target) {
+			choices.add(candidates[ind]);
+			combinationSum(ind, candidates, target - candidates[ind], answer, choices);
+			choices.remove(choices.lastIndexOf(candidates[ind]));
+			// Remove the candidate to perform proper backtracking
 		}
-
-		// pick
-		choices.add(candidates[i]);
-		combinationSum(candidates, target - candidates[i], i, answer, choices);
-
-		// don't pick
-//		choices.remove(choices.lastIndexOf(candidates[i]));
-		combinationSum(candidates, target, i + 1, answer, choices);
+		
+		// don't pick recursive case
+		combinationSum(ind + 1, candidates, target, answer, choices);
 	}
 
 }
